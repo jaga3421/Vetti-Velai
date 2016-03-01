463924244c5522b29acec9c161da5bd7b6ef99f1
Jagadeesh <jagadeesh.jkp@gmail.com>
2016-03-01 12:15:44 +0530

    Update nameList_from_linkedin.js

--- nameList_from_linkedin.js
+++ nameList_from_linkedin.js
@@ -3,14 +3,14 @@
 // i love the band cors though
 // especially the violin girl :)
 function showNames(company_name){
-  url = 'https://www.linkedin.com/vsearch/pj?keywords='+company_name+'sb=People%20who%20work%20at%'+company_name;
+  url = 'https://www.linkedin.com/vsearch/pj?keywords='+company_name+'&sb=People%20who%20work%20at%'+company_name;
 
   $.ajax({
     url: url,
     success: function(data){
       var results = data.content.page.voltron_unified_search_json.search.results;
       console.log(results.length);
-      for(var i=0;i<results.length;i++){
+      for(var i=1;i<results.length;i++){
         console.log(results[i].person.fmt_name);
         // Only 10 will be returned..
         // Get the +10 page url and poondhu vilayadify
