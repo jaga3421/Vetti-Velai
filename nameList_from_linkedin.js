fa5f9e9b7c53afe364a155179050648cfc552d31
Jagadeesh <jagadeesh.jkp@gmail.com>
2016-03-01 12:49:03 +0530

    Namelist upto 100 pages

--- nameList_from_linkedin.js
+++ nameList_from_linkedin.js
@@ -4,24 +4,22 @@
 // especially the violin girl :)
 function showNames(company_name){
   var page = 1;
-  var url = 'https://www.linkedin.com/vsearch/pj?keywords='+company_name+'&sb=People%20who%20work%20at%'+company_name+'&pageNum='+page;
-
-  $.ajax({
-    url: url,
-    success: function(data){
-      var results = data.content.page.voltron_unified_search_json.search.results;
-      console.log(results.length);
-      for(var i=1;i<results.length;i++){
-        console.log(results[i].person.fmt_name);
-        // Only 10 will be returned..
-        // Get the +10 page url and poondhu vilayadify
-        // yepdi optimise panlam
-        // naadum naatu makkalum naasamai pogattum
-        // yenna **** code adikrom
+  
+  var breakIt = false;
+  while(page < 100){
+    var url = 'https://www.linkedin.com/vsearch/pj?keywords='+company_name+'&sb=People%20who%20work%20at%'+company_name+'&page_num='+page;
+    $.ajax({
+      url: url,
+      success: function(data){
+        var results = data.content.page.voltron_unified_search_json.search.results;
+        console.log(results.length);
+        for(var i=1;i<results.length;i++){
+          console.log(results[i].person.fmt_name);        
+        }
       }
-
-    }
-  });
+    });
+    page++;
+  }
 }
 
 showNames('Freshdesk');
